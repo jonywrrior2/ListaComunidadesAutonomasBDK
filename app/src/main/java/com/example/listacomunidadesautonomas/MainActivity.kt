6731d5a52e6eec6d0085158b4f64acc0cacf53c9
cristian <cristianbersabe@gmail.com>
2023-10-09 10:10:40 +0200

    Principio listaComunidades, boton limpiar y recargar listos

--- app/src/main/java/com/example/listacomunidadesautonomas/MainActivity.kt
+++ app/src/main/java/com/example/listacomunidadesautonomas/MainActivity.kt
@@ -15,6 +15,7 @@ import com.example.listacomunidadesautonomas.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private lateinit var  datosCopia:List <Comunidad>
     
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,7 +29,7 @@ class MainActivity : AppCompatActivity() {
         binding.rvComunidad.setHasFixedSize(true)
         binding.rvComunidad.itemAnimator=DefaultItemAnimator()
 
-
+    datosCopia = ListaComunidades.listaComunidades.toList()
     }
     private fun onItemSelected(comunidad:Comunidad){
         Toast.makeText(this,"Has pulsado sobre ${comunidad.nombre}",Toast.LENGTH_SHORT).show()
@@ -43,6 +44,11 @@ class MainActivity : AppCompatActivity() {
         return when(item.itemId){
             R.id.opcionLimpiar ->{
                 ListaComunidades.listaComunidades.clear()
+                binding.rvComunidad.adapter?.notifyDataSetChanged()
+                true
+            }
+            R.id.opcionRecargar -> {
+                ListaComunidades.listaComunidades.addAll(datosCopia)
                 binding.rvComunidad.adapter?.notifyDataSetChanged()
                 true
             }
